6754387db124e06bf272c333a8f4df59c261190f
Mvjakkhil <vm0999@srmist.edi.in>
2026-04-02 14:07:50 +0530

    UC6:to print Hello, world!

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,24 @@
 public class HelloApp {
+
     public static void main(String[] args) {
 
+        // If no arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
-        } else {
-            for (String name : args) {
-                System.out.println("Hello " + name);
-            }
+            return;
         }
 
+        // Build names using StringBuilder
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        // Remove trailing ", "
+        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+        // Print final greeting
+        System.out.println("Hello, " + names + "!");
     }
 }
